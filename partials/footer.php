e3eebafc655e7cf64f4ba0313edbcea088400d1e
Bibek Lama <bibek.makura@gmail.com>
2023-12-27 17:16:24 +0545

    updates

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,101 @@
-<footer class="">
+<footer class="main-footer pt-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 nav-items">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col-md-8 logo-wrapper d-flex justify-content-center">
+                <a href="#"><img src="assets/img/logo.png" alt=""></a>
+            </div>
+            <div class="col-md-2 social-links-wrapper">
+                <span>For Questions</span>
+                <a href="tel:+91 65612791">+91 65612791</a>
+                <ul class="social-links mt-4">
+                    <li>
+                        <a href="#">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_70_39158)">
+                                    <path d="M17.4892 24.25V16.9522H19.9378L20.3052 14.1073H17.4892V12.2912C17.4892 11.4678 17.7169 10.9066 18.899 10.9066L20.4043 10.906V8.36138C20.144 8.32756 19.2504 8.25 18.2104 8.25C16.0387 8.25 14.552 9.57557 14.552 12.0094V14.1073H12.0959V16.9522H14.552V24.25H17.4892Z" fill="url(#paint0_linear_70_39158)" />
+                                </g>
+                                <circle cx="16" cy="16" r="15.5" stroke="white" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_70_39158" x1="14.9578" y1="25.2654" x2="22.6316" y2="13.5662" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F58B1F" />
+                                        <stop offset="1" stop-color="#EC2024" />
+                                    </linearGradient>
+                                    <clipPath id="clip0_70_39158">
+                                        <rect width="16" height="16" fill="white" transform="translate(8.25 8.25)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.1915 15.02L21.8541 9.6001H20.7492L16.7007 14.3061L13.4671 9.6001H9.73755L14.6273 16.7165L9.73755 22.4001H10.8425L15.1179 17.4304L18.5328 22.4001H22.2623L17.1912 15.02H17.1915ZM15.6781 16.7792L15.1827 16.0705L11.2406 10.4319H12.9378L16.119 14.9825L16.6145 15.6911L20.7497 21.6061H19.0526L15.6781 16.7794V16.7792Z" fill="url(#paint0_linear_70_39163)" />
+                                <circle cx="16" cy="16" r="15.5" stroke="white" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_70_39163" x1="14.0518" y1="23.2124" x2="18.2078" y2="11.2729" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F58B1F" />
+                                        <stop offset="1" stop-color="#EC2024" />
+                                    </linearGradient>
+                                </defs>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_70_39167)">
+                                    <path d="M18.5869 9.6001H13.4133C11.3107 9.6001 9.6001 11.3107 9.6001 13.4133V18.5869C9.6001 20.6895 11.3107 22.4001 13.4133 22.4001H18.5869C20.6895 22.4001 22.4001 20.6895 22.4001 18.5869V13.4133C22.4001 11.3107 20.6895 9.6001 18.5869 9.6001ZM21.1124 18.5869C21.1124 19.9817 19.9817 21.1124 18.5869 21.1124H13.4133C12.0185 21.1124 10.8878 19.9817 10.8878 18.5869V13.4133C10.8878 12.0185 12.0185 10.8878 13.4133 10.8878H18.5869C19.9817 10.8878 21.1124 12.0185 21.1124 13.4133V18.5869Z" fill="url(#paint0_linear_70_39167)" />
+                                    <path d="M16 12.6895C14.1746 12.6895 12.6895 14.1746 12.6895 16C12.6895 17.8254 14.1746 19.3105 16 19.3105C17.8254 19.3105 19.3105 17.8254 19.3105 16C19.3105 14.1745 17.8254 12.6895 16 12.6895ZM16 18.0228C14.8828 18.0228 13.9771 17.1172 13.9771 16C13.9771 14.8828 14.8828 13.9771 16 13.9771C17.1172 13.9771 18.0228 14.8828 18.0228 16C18.0228 17.1172 17.1172 18.0228 16 18.0228Z" fill="url(#paint1_linear_70_39167)" />
+                                    <path d="M19.3172 13.5077C19.7553 13.5077 20.1105 13.1525 20.1105 12.7144C20.1105 12.2763 19.7553 11.9211 19.3172 11.9211C18.8791 11.9211 18.5239 12.2763 18.5239 12.7144C18.5239 13.1525 18.8791 13.5077 19.3172 13.5077Z" fill="url(#paint2_linear_70_39167)" />
+                                </g>
+                                <circle cx="16" cy="16" r="15.5" stroke="white" />
+                                <defs>
+                                    <linearGradient id="paint0_linear_70_39167" x1="14.0091" y1="23.2124" x2="18.0946" y2="11.2178" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F58B1F" />
+                                        <stop offset="1" stop-color="#EC2024" />
+                                    </linearGradient>
+                                    <linearGradient id="paint1_linear_70_39167" x1="14.9701" y1="19.7307" x2="17.0834" y2="13.5262" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F58B1F" />
+                                        <stop offset="1" stop-color="#EC2024" />
+                                    </linearGradient>
+                                    <linearGradient id="paint2_linear_70_39167" x1="19.0704" y1="13.6084" x2="19.5768" y2="12.1217" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#F58B1F" />
+                                        <stop offset="1" stop-color="#EC2024" />
+                                    </linearGradient>
+                                    <clipPath id="clip0_70_39167">
+                                        <rect width="12.8" height="12.8" fill="white" transform="translate(9.6001 9.6001)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="mt-40">
+
+        </div>
+        <div class="row bottom-footer">
+            <div class="col-md-4 item">
+                <p>© Fire Kirin Website, 2023</p>
+            </div>
+            <div class="col-md-4 item text-center">
+                <p>31215 Belka Street, Texas, USA</p>
+            </div>
+            <div class="col-md-4 item text-end">
+                <p>Designed & Developed by: <a href="#">Makura Creations</a></p>
+            </div>
+        </div>
+    </div>
 </footer>
 <a href="javascript:void(0)" id="return-to-top">
     <svg viewBox="0 0 24 24" width="24" height="24" stroke="#fff" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
--- partials/header.php
+++ partials/header.php
@@ -20,6 +20,16 @@ $page = end($link_array);
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Game website</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- Caprasimo Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Caprasimo&display=swap" rel="stylesheet">
+    <!-- Onset Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Onest:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+
     <!-- Owl Carousel -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
@@ -32,3 +42,25 @@ $page = end($link_array);
     <!-- <div class="pre-loader">
         <img src="assets/img/loader3.gif" alt="">
     </div> -->
+    <header class="main-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 logo-wrapper">
+                    <a href="#">
+                        <img src="assets/img/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="col-md-7 nav-items">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 btn-wrapper">
+                    <a href="#" class="btn btn-v1 dark-bg">Download<span></span></a>
+                </div>
+            </div>
+        </div>
+    </header>
