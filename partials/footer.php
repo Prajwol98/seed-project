9ade66b96bf8a84157acb98448bc06c3da70dd38
Bibek Lama <bibek.makura@gmail.com>
2024-01-05 11:44:12 +0545

    updates

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,96 @@
+<footer class="main-footer py-60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4 about">
+                <img src="assets/img/logo.png" alt="">
+                <p>Every internet cafe games related to slots and sweepstakes</p>
+                <ul class="social-links">
+                    <li>
+                        <a href="#">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.0181 21V12.79H16.7728L17.186 9.58941H14.0181V7.54632C14.0181 6.61998 14.2743 5.98869 15.6042 5.98869L17.2975 5.98799V3.12531C17.0047 3.08725 15.9995 3 14.8295 3C12.3863 3 10.7137 4.49127 10.7137 7.22934V9.58941H7.95068V12.79H10.7137V21H14.0181Z" fill="white" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.2833 10.1137L20.403 3H18.9528L13.6391 9.17668L9.39502 3H4.5L10.9179 12.3402L4.5 19.8H5.95025L11.5617 13.2772L16.0437 19.8H20.9388L14.2829 10.1137H14.2833ZM12.297 12.4225L11.6467 11.4925L6.4728 4.09173H8.70031L12.8757 10.0644L13.526 10.9944L18.9535 18.7579H16.726L12.297 12.4229V12.4225Z" fill="white" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_271_262)">
+                                    <path d="M14.7952 3H8.00481C5.24515 3 3 5.24515 3 8.00481V14.7952C3 17.5549 5.24515 19.8 8.00481 19.8H14.7952C17.5549 19.8 19.8 17.5549 19.8 14.7952V8.00481C19.8 5.24515 17.5548 3 14.7952 3ZM18.1099 14.7952C18.1099 16.6258 16.6258 18.1099 14.7952 18.1099H8.00481C6.17415 18.1099 4.69008 16.6258 4.69008 14.7952V8.00481C4.69008 6.17412 6.17415 4.69008 8.00481 4.69008H14.7952C16.6258 4.69008 18.1099 6.17412 18.1099 8.00481V14.7952Z" fill="white" />
+                                    <path d="M11.4 7.05469C9.00413 7.05469 7.05493 9.00389 7.05493 11.3997C7.05493 13.7956 9.00413 15.7448 11.4 15.7448C13.7959 15.7448 15.7451 13.7956 15.7451 11.3997C15.7451 9.00385 13.7959 7.05469 11.4 7.05469ZM11.4 14.0548C9.93368 14.0548 8.74501 12.8661 8.74501 11.3998C8.74501 9.93344 9.93371 8.74477 11.4 8.74477C12.8663 8.74477 14.055 9.93344 14.055 11.3998C14.055 12.8661 12.8663 14.0548 11.4 14.0548Z" fill="white" />
+                                    <path d="M15.7533 8.12921C16.3284 8.12921 16.7945 7.66307 16.7945 7.08805C16.7945 6.51302 16.3284 6.04688 15.7533 6.04688C15.1783 6.04688 14.7122 6.51302 14.7122 7.08805C14.7122 7.66307 15.1783 8.12921 15.7533 8.12921Z" fill="white" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_271_262">
+                                        <rect width="16.8" height="16.8" fill="white" transform="translate(3 3)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4 list-item">
+                <div class="title">Navigation</div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Our Games</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4 list-item">
+                <div class="title">Featured Games</div>
+                <ul>
+                    <li><a href="#">Golden Wheel</a></li>
+                    <li><a href="#">Golden Dragon</a></li>
+                    <li><a href="#">Fire Kirin</a></li>
+                    <li><a href="#">Kraken</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4 list-item">
+                <div class="title">Contacts</div>
+                <ul>
+                    <li>
+                        <a href="tel:+91 215 684 6512"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M18.3706 15.1C17.8244 14.6419 14.6206 12.6131 14.0881 12.7063C13.8381 12.7506 13.6469 12.9638 13.135 13.5744C12.8983 13.8749 12.6383 14.1564 12.3575 14.4163C11.843 14.292 11.345 14.1074 10.8738 13.8663C9.0258 12.9666 7.53289 11.4732 6.63375 9.625C6.39265 9.15379 6.20804 8.65576 6.08375 8.14125C6.34358 7.86044 6.62507 7.60047 6.92563 7.36375C7.53562 6.85188 7.74938 6.66187 7.79375 6.41062C7.88688 5.87687 5.85625 2.67437 5.4 2.12813C5.20875 1.90188 5.035 1.75 4.8125 1.75C4.1675 1.75 1.25 5.3575 1.25 5.825C1.25 5.86313 1.3125 9.61875 6.05562 14.4444C10.8813 19.1875 14.6369 19.25 14.675 19.25C15.1425 19.25 18.75 16.3325 18.75 15.6875C18.75 15.465 18.5981 15.2913 18.3706 15.1Z" fill="white" />
+                                <path d="M14.375 9.875H15.625C15.6235 8.54937 15.0962 7.27847 14.1589 6.34111C13.2215 5.40375 11.9506 4.87649 10.625 4.875V6.125C11.6193 6.12599 12.5725 6.5214 13.2756 7.22445C13.9786 7.92749 14.374 8.88074 14.375 9.875Z" fill="white" />
+                                <path d="M17.5 9.875H18.75C18.7475 7.72088 17.8907 5.65569 16.3675 4.1325C14.8443 2.6093 12.7791 1.75248 10.625 1.75V3C12.4477 3.00215 14.1951 3.72717 15.484 5.01602C16.7728 6.30486 17.4978 8.0523 17.5 9.875Z" fill="white" />
+                            </svg>
+                            +91 215 684 6512</a>
+                    </li>
+                    <li>
+                        <a href="mailTo:support@icafegames.com"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_267_234)">
+                                    <path d="M19.8239 4.47266L13.7583 10.4992L19.8239 16.5257C19.9335 16.2965 20.0001 16.0432 20.0001 15.7726V5.22574C20.0001 4.95516 19.9335 4.70184 19.8239 4.47266Z" fill="white" />
+                                    <path d="M18.2421 3.46875H1.75772C1.48714 3.46875 1.23382 3.53527 1.00464 3.64492L8.7571 11.3583C9.44257 12.0438 10.5573 12.0438 11.2427 11.3583L18.9952 3.64492C18.766 3.53527 18.5127 3.46875 18.2421 3.46875Z" fill="white" />
+                                    <path d="M0.176172 4.47266C0.0665234 4.70184 0 4.95516 0 5.22574V15.7726C0 16.0432 0.0665234 16.2966 0.176172 16.5257L6.24176 10.4992L0.176172 4.47266Z" fill="white" />
+                                    <path d="M12.9296 11.3281L12.0713 12.1864C10.9291 13.3286 9.07066 13.3286 7.92851 12.1864L7.07022 11.3281L1.00464 17.3546C1.23382 17.4643 1.48714 17.5308 1.75772 17.5308H18.2421C18.5127 17.5308 18.766 17.4643 18.9952 17.3546L12.9296 11.3281Z" fill="white" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_267_234">
+                                        <rect width="20" height="20" fill="white" transform="translate(0 0.5)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            support@icafegames.com</a>
+                    </li>
 
-
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <a href="javascript:void(0)" id="return-to-top">
-    <svg viewBox="0 0 24 24" width="24" height="24" stroke="#fff" stroke-width="2" fill="none" stroke-linecap="round"
-        stroke-linejoin="round" class="css-i6dzq1">
+    <svg viewBox="0 0 24 24" width="24" height="24" stroke="#fff" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
         <polyline points="18 15 12 9 6 15"></polyline>
     </svg>
 </a>
@@ -15,9 +102,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"
-    integrity="sha512-IsNh5E3eYy3tr/JiX2Yx4vsCujtkhwl7SLqgnwLNgf04Hrt9BT9SXlLlZlWx+OK4ndzAoALhsMNcCmkggjZB1w=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js" integrity="sha512-IsNh5E3eYy3tr/JiX2Yx4vsCujtkhwl7SLqgnwLNgf04Hrt9BT9SXlLlZlWx+OK4ndzAoALhsMNcCmkggjZB1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script type="text/javascript" src="assets/js/style.js"></script>
 
