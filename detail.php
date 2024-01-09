8d88f1892cf2a3d78ac8556464c806a5f985d407
Bibek Lama <bibek.makura@gmail.com>
2024-01-09 17:36:27 +0545

    updates

---
+++ detail.php
@@ -0,0 +1,197 @@
+<?php include("partials/header.php"); ?>
+<div class="main-body">
+    <?php include("partials/sidebar.php"); ?>
+    <div class="main-content">
+
+        <section class="welcome-banner detail">
+            <img src="assets/img/game-detail-img.png" class="bg-img" alt="">
+            <div class="text-content-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <div class="text-content text-center py-120">
+                                <h1 class="font-64">Invaders from the Planet Moolah</h1>
+                                <p>Step into the realm where millionaires are born with our curated selection of casino games, where high stakes for an extraordinary gaming experience.</p>
+                                <a href="#" class="btn bordered mt-5 mx-auto">
+                                    Play Game
+                                </a>
+                                <div class="btn-wrapper mt-40">
+                                    <a href="#"><img src="assets/img/white-appstore.png" alt=""></a>
+                                    <a href="#"><img src="assets/img/white-playstore.png" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="detail-content py-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 count-wrapper">
+                        <div class="count-card">
+                            <div class="title">Yearly Transactions</div>
+                            <div class="count">516<span>B+</span></div>
+                        </div>
+                        <div class="count-card">
+                            <div class="title">Total Downloads</div>
+                            <div class="count">120<span>M+</span></div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4 img-wrapper text-center">
+                        <img src="assets/img/detail-img.png" alt="">
+                    </div>
+                    <div class="col-md-4 count-wrapper">
+                        <div class="count-card">
+                            <div class="title">Monthly Active Users</div>
+                            <div class="count">516<span>B+</span></div>
+                        </div>
+                        <div class="count-card">
+                            <div class="title">Monthly Transactions</div>
+                            <div class="count">120<span>M+</span></div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 video-wrapper">
+                        <div class="outer-div">
+                            <div class="inner-div">
+                                <div class="popup pop-up" href="https://www.youtube.com/watch?v=GXDLq-jNgVM">
+                                    <svg width="158" height="158" viewBox="0 0 158 158" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g filter="url(#filter0_d_319_97708)">
+                                            <path d="M133.367 99.1207L117.327 92.1646C112.062 89.8826 112.379 82.4006 117.819 80.5634C116.765 80.1581 115.78 79.6592 114.869 79.0829C110.559 76.3577 111.289 69.9024 116.037 68.0238L134 60.9182L116.8 65.8858C111.358 67.4564 106.582 61.6657 109.44 56.8296C109.493 56.7395 109.547 56.6495 109.602 56.5612C109.055 56.7432 108.506 56.8963 107.957 57.0205C102.494 58.2561 98.157 52.3915 100.803 47.5122L109.62 31.2514L99.5885 46.1056C96.2878 50.9939 88.6268 49.3314 87.7059 43.5245L87.684 43.3894C87.5819 43.5551 87.478 43.7172 87.3704 43.8775C84.1481 48.728 76.5691 47.2637 75.3655 41.5847L71.6381 24L73.6331 41.8584C74.2823 47.6654 67.4839 51.3433 62.8811 47.6761C62.6933 47.5266 62.5091 47.3718 62.3249 47.2133C63.2076 53.067 56.3909 56.9773 51.6805 53.3191L37.8248 42.5609L50.8927 54.897C54.9977 58.7712 52.647 65.8083 46.9829 66.2064C46.4577 66.2442 45.9271 66.2569 45.3873 66.2443C45.4383 66.2893 45.4876 66.3325 45.5386 66.3775C49.9244 70.2752 47.0249 77.5212 41.1291 77.7068L24 78.2471L40.7371 79.317C47.0012 79.7169 49.7767 87.5699 44.9095 91.4856L44.7964 91.5757C45.3471 91.5883 45.8888 91.6261 46.4213 91.6891C51.8483 92.3285 53.8543 99.1387 49.7803 102.737L36.6321 114.358L50.8289 104.059C55.2603 100.844 61.6028 104.245 61.083 109.652C61.0283 110.216 60.9445 110.783 60.8296 111.352C61.4077 110.895 62.0058 110.482 62.6204 110.113C67.2815 107.318 73.1389 111.134 72.3639 116.464L69.8145 134L73.872 117.561C75.2361 112.033 82.722 110.897 85.6708 115.783C85.7783 115.961 85.8823 116.142 85.9844 116.325C86.1996 115.266 86.5096 114.254 86.9072 113.298C88.8712 108.555 95.5565 108.153 98.292 112.507L108.017 127.982L100.391 112.741C97.8288 107.62 102.97 102.06 108.364 104.117L108.49 104.166C107.91 103.121 107.459 102.046 107.129 100.954C105.667 96.12 110.375 91.6657 115.23 93.2399L133.367 99.1207Z" fill="#00FCFE" />
+                                            <path d="M105.215 101.533C105.233 101.593 105.252 101.654 105.27 101.714C101.367 102.085 98.1687 105.241 97.7463 109.064C93.6686 106.16 87.4265 107.25 85.1618 112.295C81.799 109.819 76.9872 110.282 74.1043 113.173C72.7719 108.66 67.654 105.963 63.0143 107.709C62.2913 103.143 57.7847 100.278 53.4119 100.908C54.9694 96.7436 52.9789 91.7225 48.5425 90.1404C50.577 86.0866 48.8888 81.0389 45.0763 78.6608C48.8701 76.5924 50.7539 71.8433 49.0435 67.8544C53.8346 66.4153 56.0858 61.059 54.4083 56.6332C58.5947 57.2947 62.8646 54.7209 64.0705 50.6219C68.4858 52.2513 73.5005 49.9101 75.1333 45.6419C77.9669 49.1127 83.3047 49.891 86.987 47.1582C89.3335 50.7683 94.2216 51.9937 98.0194 50.0895C97.7263 54.8418 101.569 59.2697 106.593 59.1904C106.221 63.1093 108.952 66.7192 112.677 67.8097C108.942 71.2746 108.981 77.7262 113.8 80.7732L113.8 80.7734C113.803 80.7753 113.806 80.7771 113.809 80.779C111.187 83.5093 110.828 87.7977 112.85 90.9277C107.739 91.1624 103.572 96.1037 105.215 101.533Z" stroke="#00FCFE" stroke-width="4" />
+                                        </g>
+                                        <g filter="url(#filter1_d_319_97708)">
+                                            <path d="M128.425 97.2915L113.842 90.9679C109.056 88.8933 109.345 82.0914 114.29 80.4213C113.332 80.0528 112.437 79.5993 111.608 79.0753C107.69 76.5979 108.353 70.7294 112.67 69.0216L129 62.562L113.363 67.078C108.416 68.5058 104.075 63.2415 106.672 58.8451C106.721 58.7632 106.77 58.6814 106.82 58.6011C106.323 58.7665 105.824 58.9057 105.325 59.0187C100.358 60.1419 96.4154 54.8105 98.8209 50.3748L106.837 35.5922L97.7168 49.096C94.7162 53.5399 87.7516 52.0286 86.9144 46.7495L86.8946 46.6267C86.8017 46.7774 86.7072 46.9247 86.6094 47.0704C83.68 51.48 76.7901 50.1488 75.6959 44.9861L72.3074 29L74.121 45.235C74.7112 50.514 68.5308 53.8575 64.3465 50.5238C64.1757 50.3879 64.0083 50.2471 63.8408 50.103C64.6432 55.4246 58.4463 58.9793 54.1641 55.6538L41.568 45.8735L53.4479 57.0881C57.1797 60.6102 55.0428 67.0076 49.8936 67.3694C49.4161 67.4038 48.9337 67.4153 48.443 67.4039C48.4894 67.4448 48.5341 67.4841 48.5806 67.525C52.5676 71.0684 49.9317 77.6557 44.5719 77.8243L29 78.3155L44.2155 79.2882C49.9101 79.6517 52.4333 86.7908 48.0086 90.3505L47.9058 90.4324C48.4065 90.4439 48.8989 90.4783 49.383 90.5356C54.3166 91.1168 56.1402 97.3079 52.4367 100.579L40.4838 111.144L53.3899 101.781C57.4184 98.8585 63.1843 101.95 62.7119 106.865C62.6621 107.378 62.5859 107.894 62.4814 108.411C63.007 107.995 63.5507 107.62 64.1094 107.285C68.3468 104.743 73.6717 108.213 72.9672 113.058L70.6495 129L74.3382 114.055C75.5782 109.03 82.3836 107.997 85.0643 112.439C85.1621 112.601 85.2566 112.765 85.3495 112.932C85.5451 111.969 85.8269 111.049 86.1883 110.18C87.9738 105.868 94.0514 105.503 96.5381 109.461L105.379 123.529L98.4463 109.674C96.1171 105.018 100.79 99.9638 105.694 101.834L105.809 101.878C105.282 100.928 104.872 99.9507 104.572 98.9584C103.242 94.5636 107.523 90.5143 111.936 91.9454L128.425 97.2915Z" fill="white" />
+                                            <path d="M74.6346 109.731C73.2908 105.767 68.7726 103.419 64.6105 104.854C63.8526 100.826 59.9112 98.2882 56.0067 98.7001C57.2474 94.9427 55.4658 90.5364 51.5646 89.0272C53.2771 85.3802 51.8151 80.9359 48.5075 78.7018C51.8019 76.7401 53.4348 72.5623 52.0127 68.9815C56.2405 67.5923 58.2557 62.8862 56.9156 58.8876C60.6484 59.3297 64.3848 57.0478 65.5466 53.437C69.509 54.7895 73.9382 52.7567 75.5411 49.0222C78.1801 52.0053 82.8805 52.6601 86.2142 50.3117C88.3774 53.4631 92.6623 54.5656 96.096 53.0121C96.0032 57.2524 99.4152 61.1304 103.885 61.174C103.653 64.6442 106.018 67.8138 109.27 68.9078C106.064 72.1342 106.143 77.8311 110.363 80.6514C108.128 83.1224 107.794 86.8553 109.459 89.682C104.859 90.0776 101.182 94.5587 102.644 99.4923C99.2131 99.9208 96.4095 102.66 95.899 106.013C92.161 103.565 86.665 104.563 84.5293 108.992C81.4937 106.892 77.2838 107.276 74.6346 109.731ZM74.6346 109.731C74.9132 110.553 75.0554 111.445 75.0296 112.386C76.8595 109.397 81.1067 108.714 83.7907 110.947C83.9501 110.424 84.1336 109.912 84.3405 109.414" stroke="#073579" stroke-width="4" />
+                                        </g>
+                                        <g filter="url(#filter2_d_319_97708)">
+                                            <path d="M75 74.5455V93L92 79L75 65" stroke="#073579" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                                        </g>
+                                        <defs>
+                                            <filter id="filter0_d_319_97708" x="0" y="0" width="158" height="158" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                <feOffset />
+                                                <feGaussianBlur stdDeviation="12" />
+                                                <feComposite in2="hardAlpha" operator="out" />
+                                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.988235 0 0 0 0 0.996078 0 0 0 1 0" />
+                                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_319_97708" />
+                                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_319_97708" result="shape" />
+                                            </filter>
+                                            <filter id="filter1_d_319_97708" x="29" y="29" width="100" height="100" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                <feOffset />
+                                                <feComposite in2="hardAlpha" operator="out" />
+                                                <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0" />
+                                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_319_97708" />
+                                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_319_97708" result="shape" />
+                                            </filter>
+                                            <filter id="filter2_d_319_97708" x="71" y="61" width="25" height="36" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                                <feOffset />
+                                                <feGaussianBlur stdDeviation="1" />
+                                                <feComposite in2="hardAlpha" operator="out" />
+                                                <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0" />
+                                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_319_97708" />
+                                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_319_97708" result="shape" />
+                                            </filter>
+                                        </defs>
+                                    </svg>
+
+                                </div>
+                                <img src="assets/img/video-sec.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="screenshots-wrapper py-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 section-heading text-center mb-40">
+                        <h2 class="text-primary">Some of the user’s Screenshots</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="img-wrapper px-3" id="lightgallery">
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game1.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game2.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game3.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game4.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game1.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game2.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game3.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game4.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game1.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game2.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game3.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game4.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game1.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game3.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game2.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game4.png" alt="">
+                </div>
+                <div class="item" data-category="csw" data-src="assets/img/game1.png">
+                    <img src="assets/img/game1.png" alt="">
+                </div>
+
+            </div>
+        </section>
+
+        <section class="notice-banner py-60 mb-40">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="content-wrapper section-heading text-center py-40 px-5 position-centered flex-column bg-cs-light">
+                            <h2>Play. Win. Earn. </h2>
+                            <p>Step into the realm where millionaires are born with our curated selection of casino games, where high stakes for an extraordinary gaming experience.</p>
+                            <a class="btn bordered" href="contact.php">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+</div>
+<?php include("partials/footer.php"); ?>
