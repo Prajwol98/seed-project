0e5b1ae1b1ca35445a264099caf5de65652b0a38
Rupak <rupak.makura@gmail.com>
2024-01-12 20:43:46 +0545

    updates

---
+++ detail.php
@@ -0,0 +1,56 @@
+<?php include("partials/header.php"); ?>
+<section class="detail-page-top-wrapper">
+    <img src="assets/img/game-detail-banner.jpg" alt="game-detail-banner">
+    <div class="pt-40 top-info-wrapper">
+        <div class="container">
+            <div class="pb-80 row">
+                <div class="col-lg-4">
+                    <img src="assets/img/detail-picture.svg" alt="detail_banner_picture">
+                </div>
+                <div class="col-lg-8">
+                    <h3>Vampire Queen</h3>
+                    <p>Immerse yourself in the mysterious realm of 'Vampire Queen,' where darkness meets desire. Unearth
+                        thrilling narratives, bewitching visuals, and strategic gameplay as you navigate through the
+                        nocturnal kingdom. Will you succumb to the allure of eternal power, or defy the shadows and
+                        emerge
+                        as the ultimate victor?</p>
+                    <div class="btn-wrapper">
+                        <a href="#" class="btn primary me-3">Play Now</a>
+                        <a href="#" class="btn primary">
+                            <svg width="20" height="21" viewBox="0 0 20 21" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10 4L10 16" stroke="white" stroke-width="2" stroke-linecap="round" />
+                                <path d="M14 12L10 16" stroke="white" stroke-width="2" stroke-linecap="round" />
+                                <path d="M6 12L10 16" stroke="white" stroke-width="2" stroke-linecap="round" />
+                            </svg>
+                            Download This Game
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="py-60 screenshot-section">
+        <div class="container">
+            <div class="row">
+                <h3>Screenshots on Vampire Queen</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ready-to-play-cta">
+    <div class="container">
+        <div class="row">
+            <div class="inner-content-wrapper">
+                <img src="assets/img/cta-banner.svg" class="cta-banner" alt="cta_banner_image">
+                <div class="col-lg-6">
+                    <h3>Ready to play and win?</h3>
+                    <p>Explore the step-by-step guide to set up your account and start your gaming adventure.</p>
+                    <a href="#" class="btn primary">Download Game</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include("partials/footer.php"); ?>
